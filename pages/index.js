ef970fb4737ee5b47503bf812d84efe48b4ef6c2
Sterbenfr <sterben538@gmail.com>
2023-05-31 12:32:00 +0200

    Typos(index):Fixed some typo

--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,18 @@ export default function Home() {
         </p>
         <p>
           Je suis en première année d'un Bac +3 et je cherche actuellement un stage,
-          de préférence en tant que dévelopeur FullStack/ BackEnd. Il est de deux mois minimum
+          de préférence en tant que développeur FullStack/ BackEnd. Il est de deux mois minimum
           et trois mois maximum. Je peux commencer dès le 12 juin.
         </p>
         <p>
           Je suis très engagé dans mes projets que je conduis volontier en équipe,
-          je posséde une grande créativité.
+          je possède une grande créativité.
         </p>
         <p>
           Je suis passionné par les minéraux et fossiles, je fais parti d'un club basé à Wasquehal
           (<Link target="_blank" href="https://www.instagram.com/sperkise/">@sperkise</Link>)
           avec lequel je fais des salons et des sorties terrains.
-          À coté de cette passion, je me suis intéressé au dévellopement et j'ai commencé à apprendre
+          À coté de cette passion, je me suis intéressé au développement et j'ai commencé à apprendre
           le python. J'ai ensuite suivi une option informatique en seconde puis, plus tard, intégré mon
           école actuelle, ENIGMA.
           Je fais de la natation et j'aime le monde de la musique en général.
